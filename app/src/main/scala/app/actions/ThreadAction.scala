d3a9c96fa2014a74364a5b1ec3a0fbcf3b411d5b
Takeharu.Oshida <georgeosddev@gmail.com>
2015-05-26 12:38:07 +0900

    Add ThreadAction

--- app/src/main/scala/app/actions/ThreadAction.scala
+++ app/src/main/scala/app/actions/ThreadAction.scala
@@ -1,48 +1,94 @@
 package app.actions
 
-import xitrum.annotation.{GET, POST, PUT, DELETE, Swagger}
+import xitrum.annotation.{GET, POST, PUT, DELETE, First, Swagger}
 
+import app.constants.ErrorCds
+import app.models.Thread
+import app.validators.AppValidator
+
+
+@Swagger(
+  Swagger.Resource("threads", "APIs to list threads"),
+  Swagger.StringBody("body", "email"),
+  Swagger.StringBody("token", "token"),
+  Swagger.OptIntQuery("limit", "limit count"),
+  Swagger.OptIntQuery("skip", "Skip count"),
+  Swagger.OptStringQuery("sort", "Sort key"),
+  Swagger.OptStringQuery("tags", "tags to search as csv, eg: tag1,tag2"),
+  Swagger.OptStringQuery("keyword", "keyword to search"),
+  Swagger.Produces("application/json"),
+  Swagger.Response(200, "JSON"),
+  Swagger.Response(400, "Wrong token etc")
+)
 @GET("/api/:version/threads")
 class ListThreads extends BaseAction with APIAction {
   def execute(){
     
-    val tags    = paramo[String]("tags").getOrElse("").split(',')
-    val keyword = paramo[String]("q")
+    val tags    = paramo[String]("tags").map(_.split(',')).getOrElse(Array.empty)
+    val keyword = paramo[String]("keyword")
 
-    val limit = paramo[Int]("limit").getOrElse(100)
-    val skip  = paramo[Int]("offset").getOrElse(0)
-    val sort  = paramo[String]("skip").getOrElse("_id")
+    val limit = paramo[Int]("limit")
+    val skip  = paramo[Int]("offset")
+    val sort  = paramo[String]("skip")
 
-
-    respondSuccess(Map("users" -> Thread.listAll(tags, keyword, limit, skip, sort).map(_.toMap)))
+    respondSuccess(Map("threads" -> Thread.listAll(tags, keyword, limit, skip, sort).map(_.toMap)))
   }
 }
 
 @POST("/api/:version/threads")
 class CreateThread extends BaseAction with APIAction {
   def execute(){
-    
+
+    val reqJSON = requestContentJson[Map[String, String]].getOrElse(Map.empty)
+    val titleo = reqJSON.get("title")
+    val tagso  = reqJSON.get("tags")
+
+    if (!titleo.isDefined) {
+      respondClientError(ErrorCds.MISSING_PARAM, "title")
+      return
+    } else {
+      val isValidTitleMinLength = AppValidator.minLength(AppValidator.TITLE_MINLENGTH)
+      val isValidTitleMaxLength = AppValidator.maxLength(AppValidator.TITLE_MAXLENGTH)
+      if (!isValidTitleMinLength(titleo.get) || !isValidTitleMaxLength(titleo.get)) {
+        respondClientError(ErrorCds.INVALID_PARAM, "title")
+        return
+      }
+    }
+    val tags = tagso.getOrElse("").split(',')
+    titleo.map{ title => 
+      Thread.create(title, tags, at("currentUser")) match {
+        case true =>
+          respondSuccess(Map.empty)
+        case false =>
+          respondServerError(ErrorCds.SYSTEM_ERROR, "Failed to create thread")
+       }
+    }
   }
 }
 
 @GET("/api/:version/threads/:tid")
 class ShowThread extends BaseAction with APIAction {
   def execute(){
-    
+    val tid    = param[String]("tid")
+    Thread.findById(tid) match {
+      case None =>
+        respondClientError(ErrorCds.DATA_NOT_FOUND, "Thread not found")
+      case Some(t) =>
+        respondSuccess(t.toMap)
+    }
   }
 }
 
 @PUT("/api/:version/threads/:tid")
 class UpdateThread extends BaseAction with APIAction {
   def execute(){
-    
+    respondText("Not implemented")
   }
 }
 
 @DELETE("/api/:version/threads/:tid")
 class DeleteThread extends BaseAction with APIAction {
     def execute(){
-    
+    respondText("Not implemented")
   }
-
 }
--- app/src/main/scala/app/models/Thread.scala
+++ app/src/main/scala/app/models/Thread.scala
@@ -1,44 +1,105 @@
 package app.models
 
+
+import scala.collection.mutable.ListBuffer
+
 import com.mongodb.casbah.Imports._
 import app.service.DB
+import app.service.DB.usersColl.{T => UserType}
+import app.service.DB.threadsColl.{T => ThreadType}
 
 
-class Thread(doc: MongoDBObject) extends BaseModel {
+class Thread(doc: ThreadType) extends BaseModel {
   
-  lazy val id             = doc.getAsOrElse[String]("_id", "")
+  lazy val id             = doc.getAs[ObjectId]("_id")
+  lazy val title          = doc.getAsOrElse[String]("title", "")
+  lazy val tags           = doc.getAsOrElse[List[String]]("tags", List.empty)
+  lazy val postsCount     = doc.getAsOrElse[Int]("postsCount", 0)
 
-  
+  lazy val createdBy = doc.getAs[UserType]("createdBy").map(new User(_))
+  lazy val createdAt = doc.getAsOrElse[Int]("createdAt", 0)
+  lazy val updatedAt = doc.getAsOrElse[Int]("updatedAt", 0)
+  lazy val deletedAt = doc.getAsOrElse[Int]("deletedAt", 0)
+
+    
   override def toJson = {
     s"""{"tid":"${id}}"""
   }
   
   override def toMap = Map(
-    "tid" -> id
+    "tid" -> id.toString,
+    "title" -> title,
+    "tags" -> tags,
+    "postsCount" -> postsCount,
+    "createdBy" -> createdBy.map(_.toMap).getOrElse(Map.empty)
   )
 }
 
 object Thread {
-  private val col = DB.threadsColl
-
-  private def findById(id: String): Option[Thread] = {
-    None
-  }
-  
-  private def findByAddress(address: String): Option[Thread] = {
-    None
-  }
-
-  def listAll(tag: Seq[String], q: Option[String], limit: Int, skip: Int, sort: String): List[User] = {
-  }
 
   
-  def searchByKeyWord(keyword: String): List[Thread] = {
-    List.empty
+  private def findOne(key: String, value: AnyRef): Option[Thread] = {
+    val q = MongoDBObject(key -> value)
+    DB.threadsColl.findOne(q) match {
+      case Some(x) => Option(new Thread(x))
+      case None    => None
+    }
   }
 
-  def searchByTag(tags: Seq[String]): List[Thread] = {
-    List.empty
+  def findById(id: String): Option[Thread] = {
+    findOne("_id", new ObjectId(id))
+  }
+
+  
+  private def findByUser(email: String): Option[Thread] = {
+    None
+  }
+
+  def listAll(tags: Seq[String], q: Option[String], limit: Option[Int], skip: Option[Int], sort: Option[String]): List[Thread] = {
+    val builder = MongoDBObject.newBuilder
+    if (!tags.isEmpty) builder += "tags" -> MongoDBObject("$all" -> tags) // or use ("$in" -> tags)
+    if (q.isDefined)   builder += "title" -> MongoDBObject("$regex" -> ("^"+q.get +".*").r)
+
+    val cursor = DB.threadsColl.find(builder.result)
+                             .sort(MongoDBObject(sortableKeyOrID(sort ) -> 1))
+                             .skip(skip.getOrElse(0))
+                             .limit(limit.getOrElse(100))
+    val ret    = new ListBuffer[Thread]
+    while (cursor.hasNext) {
+      ret += new Thread(cursor.next())
+    }
+    ret.toList
+  }
+
+  def searchByKeyWord(keyword: String, limit: Option[Int], skip: Option[Int], sort: Option[String]): List[Thread] = {
+    listAll(Seq.empty, Option(keyword), limit, skip, sort)
+  }
+
+  def searchByTag(tags: Seq[String], limit: Option[Int], skip: Option[Int], sort: Option[String]): List[Thread] = {
+    listAll(tags, None, limit, skip, sort)
+  }
+
+  def create(title:String, tags:Seq[String], createdBy: User) = {
+    val o = MongoDBObject(
+      "title"     -> title,
+      "tags"      -> tags,
+      "createdBy" -> createdBy.toMap,
+      "createdAt" -> DB.nowSecs(),
+      "postsCount" -> 0
+    )
+    DB.insertIfNonexistent(DB.threadsColl, o)
+  }
+
+  private def sortableKeyOrID(keyo: Option[String]) = {
+    keyo match {
+      case Some(key) =>
+        if (key == "title" || key == "createdBy")
+          key
+        else
+          "_id"
+      case _ =>
+        "_id"
+    }
   }
 
 }
